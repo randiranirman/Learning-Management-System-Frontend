27239596b2f95140ad21ac82f791599be35f9969
Randira nirman <randiranirman20011210@gmail.com>
2024-12-24 17:12:42 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Dashboard from "./Components/Dashboard/Dashboard"
+import Sidebar from "./Components/Sidebar/Sidebar"
+
+const App = () => {
+  return (
+   <>
+        <Sidebar />
+        <Dashboard />
+   </>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,31 @@
+
+import "./Sidebar.css";
+import { useState } from "react";
+const Sidebar = () => {
+
+    const [ active , setActive ] = useState('dashboard');
+
+
+  return (
+    
+
+    <>
+            <div className="sidebar">
+                <h2>Admin Panel</h2>
+                <ul>
+                    <li className="dashboard" onClick={ ()  => setActive("dashboard")}>DashBoard</li>
+                    <li className="manage users" onClick={() => setActive("manage users")} >Manage Users</li>
+                    <li className="manage assignments"  onClick={() => setActive("manage assignments")}> Manage Assignments</li>
+                    <li className="manage courses" onClick={() => setActive("manage courses")} >Manage Courses</li>
+                    <li className="analytics" onClick={() => setActive("analytics")}>Analytics</li>
+                    <li className="settings"onClick={() => setActive("settings")} >Settings</li>
+                    <li className="log out"onClick={() => setActive("log out")} >Log Out</li>
+                </ul>
+            </div>
+
+
+    </>
+  )
+}
+
+export default Sidebar
