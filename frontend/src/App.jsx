61fc4d6e2fb889bd5178c809f582314ca619e9a1
Randira nirman <randiranirman20011210@gmail.com>
2024-12-28 15:03:36 +0530

    implementaion of tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
+
+import "./index.css"
 import Dashboard from "./Components/Dashboard/Dashboard"
 import Sidebar from "./Components/Sidebar/Sidebar"
 
 const App = () => {
   return (
    <>
+      <h2 className="text-4xl font-bold 500">Hello World </h2>
         <Sidebar />
         <Dashboard />
    </>
