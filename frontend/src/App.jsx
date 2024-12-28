ea00f6d5333e020f9e5f0417c2386d9256ac9fa3
Randira nirman <randiranirman20011210@gmail.com>
2024-12-28 10:50:25 +0530

    tailwind css implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import Sidebar from "./Components/Sidebar/Sidebar"
 
 const App = () => {
   return (
+
    <>
+        <h1 className="text -3xl font-bold underline">This is tailwind css</h1>
         <Sidebar />
         <Dashboard />
    </>
